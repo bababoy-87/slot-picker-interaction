9ebc7828f8aeef020783287b13f5841ab2ec48c7
mirayatech <mirayaabrodi@gmail.com>
2025-06-17 20:43:58 +0200

    feat: update App component to include Schedule for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Schedule } from "./components/Schedule";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="relative w-full min-h-screen flex items-start md:items-center justify-center px-4 py-10 bg-zinc-50">
+      <Schedule />
+    </main>
+  );
 }
